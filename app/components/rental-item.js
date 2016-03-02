8334cd635ffd45e4b12cd0b9d83953e4283fdc68
Duong Tam Chau <dtc@iuservice.com>
2016-03-02 14:45:44 -0500

    Basic model.

--- app/components/rental-item.js
+++ app/components/rental-item.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  isImageShowing: false,
+  actions: {
+    showImage() {
+      this.set('isImageShowing', true);
+    },
+    hideImage() {
+      this.set('isImageShowing', false);
+    }
+  }
 });
